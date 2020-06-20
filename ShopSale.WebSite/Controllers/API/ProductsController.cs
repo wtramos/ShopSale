89dfdbfb046fbf1040421e52728b0d004d3ab4ac
wtramos <wtramos@hotmail.com>
2020-06-19 21:08:14 -0500

    API Products Controller

---
+++ ShopSale.WebSite/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿namespace ShopSale.WebSite.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Data.Interfaces;
+
+    [Route("api/[Controller]")]
+	public class ProductsController : Controller
+	{
+		private readonly IProductRepository _productRepository;
+
+		public ProductsController(IProductRepository productRepository)
+		{
+			this._productRepository = productRepository;
+		}
+
+		[HttpGet]
+		public IActionResult GetProducts()
+		{
+			return this.Ok(this._productRepository.GetAll());
+		}
+	}
+}
--- ShopSale.WebSite/Data/Entities/Country.cs
+++ ShopSale.WebSite/Data/Entities/Country.cs
@@ -1,8 +1,13 @@
 ﻿namespace ShopSale.WebSite.Data.Entities
 {
+    using System.ComponentModel.DataAnnotations;
     public class Country : IEntity
     {
         public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        [Required]
+        [Display(Name = "Country")]
         public string Name { get; set; }
     }
 }
