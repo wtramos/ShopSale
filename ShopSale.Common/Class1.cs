f36559643ad1fd531016859335967be1cc458557
wtramos <wtramos@hotmail.com>
2020-06-21 20:18:10 -0500

    the Solution Xamarin

---
+++ ShopSale.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShopSale.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ ShopSale.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShopSale.UIClassic
+{
+    public class Class1
+    {
+    }
+}
