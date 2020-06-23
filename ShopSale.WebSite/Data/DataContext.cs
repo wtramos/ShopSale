0b3ae6325b27555515253e9416bc49b6849c39d5
wtramos <wtramos@hotmail.com>
2020-06-22 19:56:18 -0500

    To Disable Cascade Delete Rule & Avoid Warnings in Update Database

--- ShopSale.WebSite/Data/DataContext.cs
+++ ShopSale.WebSite/Data/DataContext.cs
@@ -1,9 +1,10 @@
 ﻿namespace ShopSale.WebSite.Data
 {
+	using System.Linq;
 	using Microsoft.EntityFrameworkCore;
 	using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 	using ShopSale.WebSite.Data.Entities;
-
+   
     public class DataContext : IdentityDbContext<User>
 	{
 		public DbSet<Product> Products { get; set; }
@@ -12,5 +13,24 @@
         public DataContext(DbContextOptions<DataContext> options) : base(options)
 		{
 		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>()
+				.Property(p => p.Price)
+				.HasColumnType("decimal(18,2)");
+
+			var cascadeFKs = modelBuilder.Model
+				.G­etEntityTypes()
+				.SelectMany(t => t.GetForeignKeys())
+				.Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Casca­de);
+			foreach (var fk in cascadeFKs)
+			{
+				fk.DeleteBehavior = DeleteBehavior.Restr­ict;
+			}
+
+			base.OnModelCreating(modelBuilder);
+		}
+
 	}
 }
