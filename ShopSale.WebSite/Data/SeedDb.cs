0eaecc666a5e41b3c96c51d43d2e5752304ea16e
wtramos <wtramos@hotmail.com>
2020-06-24 21:26:28 -0500

    Improve the Seeder

--- ShopSale.WebSite/Data/SeedDb.cs
+++ ShopSale.WebSite/Data/SeedDb.cs
@@ -3,98 +3,147 @@
     using System;
     using System.Linq;
     using System.Threading.Tasks;
-    using System.Collections.Generic;
-    using Microsoft.AspNetCore.Identity;
     using Entities;
     using Helpers;
-    
+    using Microsoft.AspNetCore.Identity;
+
     public class SeedDb
     {
-        private readonly DataContext _context;
-        private readonly IUserHelper _userHelper;
-        private Random _random;
+        private readonly DataContext context;
+        private readonly IUserHelper userHelper;
+        private readonly Random random;
 
         public SeedDb(DataContext context, IUserHelper userHelper)
         {
-            this._context = context;
-            this._userHelper = userHelper;
-            this._random = new Random();
+            this.context = context;
+            this.userHelper = userHelper;
+            this.random = new Random();
         }
+
         public async Task SeedAsync()
         {
-            await this._context.Database.EnsureCreatedAsync();
+            await this.context.Database.EnsureCreatedAsync();
 
-            await this._userHelper.CheckRoleAsync("Admin");
-            await this._userHelper.CheckRoleAsync("Customer");
+            await this.CheckRoles();
 
-            if (!this._context.Countries.Any())
+            if (!this.context.Countries.Any())
             {
-                var cities = new List<City>();
-                cities.Add(new City { Name = "Lima" });
-                cities.Add(new City { Name = "Ica" });
-                cities.Add(new City { Name = "Huancayo" });
-                cities.Add(new City { Name = "Piura" });
-
-                this._context.Countries.Add(new Country
-                {
-                    Cities = cities,
-                    Name = "Perú"
-                });
-
-                await this._context.SaveChangesAsync();
+                await this.AddCountriesAndCitiesAsync();
             }
 
-            var user = await this._userHelper.GetUserByEmailAsync("walter.torres.ramos@gmail.com");
-            if (user == null)
+            await this.CheckUserAsync("brad@gmail.com", "Brad", "Pit", "Customer");
+            await this.CheckUserAsync("angelina@gmail.com", "Angelina", "Jolie", "Customer");
+            var user = await this.CheckUserAsync("walter.torres.ramos@gmail.com", "Walter", "Torres", "Admin");
+
+            // Add products
+            if (!this.context.Products.Any())
             {
-                user = new User
-                {
-                    FirstName = "Walter",
-                    LastName = "Torres",
-                    Email = "walter.torres.ramos@gmail.com",
-                    UserName = "walter.torres.ramos@gmail.com",
-                    PhoneNumber = "938252115",
-                    Address = "Calle Santa Carmela 118 - Surco",
-                    CityId = this._context.Countries.FirstOrDefault().Cities.FirstOrDefault().Id,
-                    City = this._context.Countries.FirstOrDefault().Cities.FirstOrDefault()
-
-                };
-
-                var result = await this._userHelper.AddUserAsync(user, "123456");
-                if (result != IdentityResult.Success)
-                {
-                    throw new InvalidOperationException("Could not create the user in seeder");
-                }
-                await this._userHelper.AddUserToRoleAsync(user, "Admin");
-                var token = await this._userHelper.GenerateEmailConfirmationTokenAsync(user);
-                await this._userHelper.ConfirmEmailAsync(user, token);
-            }
-
-            var isInRole = await this._userHelper.IsUserInRoleAsync(user, "Admin");
-            if (!isInRole)
-            {
-                await this._userHelper.AddUserToRoleAsync(user, "Admin");
-            }
-
-
-            if (!this._context.Products.Any())
-            {
-                this.AddProduct("First Product", user);
-                this.AddProduct("Second Product", user);
-                this.AddProduct("Third Product", user);
-                await this._context.SaveChangesAsync();
+                this.AddProduct("AirPods", 159, user);
+                this.AddProduct("Blackmagic eGPU Pro", 1199, user);
+                this.AddProduct("iPad Pro", 799, user);
+                this.AddProduct("iMac", 1398, user);
+                this.AddProduct("iPhone X", 749, user);
+                this.AddProduct("iWatch Series 4", 399, user);
+                this.AddProduct("Mac Book Air", 789, user);
+                this.AddProduct("Mac Book Pro", 1299, user);
+                this.AddProduct("Mac Mini", 708, user);
+                this.AddProduct("Mac Pro", 2300, user);
+                this.AddProduct("Magic Mouse", 47, user);
+                this.AddProduct("Magic Trackpad 2", 140, user);
+                this.AddProduct("USB C Multiport", 145, user);
+                this.AddProduct("Wireless Charging Pad", 67.67M, user);
+                await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name, User user)
+        private async Task<User> CheckUserAsync(string userName, string firstName, string lastName, string role)
         {
-            this._context.Products.Add(new Product
+            // Add user
+            var user = await this.userHelper.GetUserByEmailAsync(userName);
+            if (user == null)
+            {
+                user = await this.AddUser(userName, firstName, lastName, role);
+            }
+
+            var isInRole = await this.userHelper.IsUserInRoleAsync(user, role);
+            if (!isInRole)
+            {
+                await this.userHelper.AddUserToRoleAsync(user, role);
+            }
+
+            return user;
+        }
+
+        private async Task<User> AddUser(string userName, string firstName, string lastName, string role)
+        {
+            var user = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = userName,
+                UserName = userName,
+                Address = "Calle Luna Calle Sol",
+                PhoneNumber = "938252115",
+                CityId = this.context.Countries.FirstOrDefault().Cities.FirstOrDefault().Id,
+                City = this.context.Countries.FirstOrDefault().Cities.FirstOrDefault()
+            };
+
+            var result = await this.userHelper.AddUserAsync(user, "123456");
+            if (result != IdentityResult.Success)
+            {
+                throw new InvalidOperationException("Could not create the user in seeder");
+            }
+
+            await this.userHelper.AddUserToRoleAsync(user, role);
+            var token = await this.userHelper.GenerateEmailConfirmationTokenAsync(user);
+            await this.userHelper.ConfirmEmailAsync(user, token);
+            return user;
+        }
+
+        private async Task AddCountriesAndCitiesAsync()
+        {
+            this.AddCountry("Colombia", new string[] { "Medellín", "Bogota", "Calí", "Barranquilla", "Bucaramanga", "Cartagena", "Pereira" });
+            this.AddCountry("Argentina", new string[] { "Córdoba", "Buenos Aires", "Rosario", "Tandil", "Salta", "Mendoza" });
+            this.AddCountry("Estados Unidos", new string[] { "New York", "Los Ángeles", "Chicago", "Washington", "San Francisco", "Miami", "Boston" });
+            this.AddCountry("Ecuador", new string[] { "Quito", "Guayaquil", "Ambato", "Manta", "Loja", "Santo" });
+            this.AddCountry("Peru", new string[] { "Lima", "Arequipa", "Cusco", "Trujillo", "Chiclayo", "Iquitos" });
+            this.AddCountry("Chile", new string[] { "Santiago", "Valdivia", "Concepcion", "Puerto Montt", "Temucos", "La Sirena" });
+            this.AddCountry("Uruguay", new string[] { "Montevideo", "Punta del Este", "Colonia del Sacramento", "Las Piedras" });
+            this.AddCountry("Bolivia", new string[] { "La Paz", "Sucre", "Potosi", "Cochabamba" });
+            this.AddCountry("Venezuela", new string[] { "Caracas", "Valencia", "Maracaibo", "Ciudad Bolivar", "Maracay", "Barquisimeto" });
+            this.AddCountry("Paraguay", new string[] { "Asunción", "Ciudad del Este", "Encarnación", "San  Lorenzo", "Luque", "Areguá" });
+            this.AddCountry("Brasil", new string[] { "Rio de Janeiro", "São Paulo", "Salvador", "Porto Alegre", "Curitiba", "Recife", "Belo Horizonte", "Fortaleza" });
+            this.AddCountry("Panamá", new string[] { "Chitré", "Santiago", "La Arena", "Agua Dulce", "Monagrillo", "Ciudad de Panamá", "Colón", "Los Santos" });
+            this.AddCountry("México", new string[] { "Guadalajara", "Ciudad de México", "Monterrey", "Ciudad Obregón", "Hermosillo", "La Paz", "Culiacán", "Los Mochis" });
+            await this.context.SaveChangesAsync();
+        }
+
+        private void AddCountry(string country, string[] cities)
+        {
+            var theCities = cities.Select(c => new City { Name = c }).ToList();
+            this.context.Countries.Add(new Country
+            {
+                Cities = theCities,
+                Name = country
+            });
+        }
+
+        private async Task CheckRoles()
+        {
+            await this.userHelper.CheckRoleAsync("Admin");
+            await this.userHelper.CheckRoleAsync("Customer");
+        }
+
+        private void AddProduct(string name, decimal price, User user)
+        {
+            this.context.Products.Add(new Product
             {
                 Name = name,
-                Price = this._random.Next(100),
+                Price = price,
                 IsAvailabe = true,
-                Stock = this._random.Next(100),
-                User = user
+                Stock = this.random.Next(100),
+                User = user,
+                ImageUrl = $"~/images/Products/{name}.png"
             });
         }
     }
